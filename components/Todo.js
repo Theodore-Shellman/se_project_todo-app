f0697158c44753ae56c3e8214be43756b09459d3
theo shellman <theoshellman@gmail.com>
2025-03-19 11:06:19 -0400

    add elements

--- components/Todo.js
+++ components/Todo.js
@@ -1,6 +1,19 @@
-class todo {
-    constructor(data, selector) {
-        console.log(data);
-        console.log(selector);
-}};
-export default todo;
+class Todo {
+  constructor(data, selector) {
+    this._data = data;
+    this._templateElement = document.querySelector(selector);
+  }
+  getView() {
+    const todoElement = this._templateElement.content
+      .querySelector(".todo")
+      .cloneNode(true);
+
+    const todoNameEl = todoElement.querySelector(".todo__name");
+    const todoCheckboxEl = todoElement.querySelector(".todo__completed");
+    const todoLabel = todoElement.querySelector(".todo__label");
+    const todoDate = todoElement.querySelector(".todo__date");
+    const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
+    return todoElement;
+  }
+}
+export default Todo;
